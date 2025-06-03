8bb929ab320e053d328fc3d1b82ae7d703a623c3
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-06-03 21:04:03 +0800

    colored triangled

--- src/main.c
+++ src/main.c
@@ -26,25 +26,35 @@ INLINE COLOR RGB15(u32 red, u32 green, u32 blue) {
   return red | (green<<5) | (blue<<10);
 }
 
+INLINE u32 c_mix(u32 r, u32 l, u32 t, u32 len) {
+  return (((l * t) + (r * (len - t))) / len);
+}
+
 int main()
 {
     REG_DISPCNT= 0x0003 | 0x0400; // DCNT_MODE3 | DCNT_BG2
 
-    int startX = HALF_WIDTH;
-    int startY = HALF_HEIGHT - 32;
+    const int startX = HALF_WIDTH;
+    const int startY = HALF_HEIGHT - 32;
 
     for (int currY = 0; currY < 62; currY++) {
-      int startXL = startX - currY;
-      int startXR = startX + currY;
+      const int startXL = startX - currY;
+      const int startXR = startX + currY;
 
-      for (int currX = startXL; currX < startXR; currX++) {
-        m3_plot(currX, startY+currY, RGB15(31, 0, 0) );
+      u32 cLR = c_mix(31,  0, currY, 62);
+      u32 cLG = c_mix( 0, 31, currY, 62);
+      u32 cLB = c_mix( 0,  0, currY, 62);
+      u32 cRR = c_mix(31,  0, currY, 62);
+      u32 cRG = c_mix( 0,  0, currY, 62);
+      u32 cRB = c_mix( 0, 31, currY, 62);
+
+      for (int currX = 0; currX < (startXR - startXL); currX++) {
+        u32 currR = c_mix(cLR, cRR, currX, (startXR - startXL));
+        u32 currG = c_mix(cLG, cRG, currX, (startXR - startXL));
+        u32 currB = c_mix(cLB, cRB, currX, (startXR - startXL));
+        m3_plot(startXL+currX, startY+currY, RGB15(currR, currG, currB));
       }
     }
-
-    // m3_plot( 120, 80, RGB15(31, 0, 0) );
-    // m3_plot( 136, 80, RGB15( 0,31, 0) );
-    // m3_plot( 120, 96, RGB15( 0, 0,31) );
 
     while(1);
 
